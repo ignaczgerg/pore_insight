6b65f1f2e24a7d73eda0cc796e7c861d258b5ad7
Gergo Ignacz <gergo.ignacz@kaust.edu.sa>
2024-10-16 13:49:46 +0300

    cleanup

--- diffusivity.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-class DiffusivityCalculator:
-    @staticmethod
-    def bulk_diffusivity(molar_volume, molecular_weight, temp, viscosity, alpha):
-        return (7.4E-8) * temp * np.sqrt(alpha * molecular_weight) / (viscosity * (alpha * molar_volume)**0.6)
