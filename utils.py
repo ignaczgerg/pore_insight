8e3f30559bddbac59100efbbd84d196a678da9c5
Kenia Moreno <kenia.morenomartinez@kaust.edu.sa>
2024-10-31 16:26:01 +0300

    clean up

--- utils.py
+++ utils.py
@@ -2,22 +2,22 @@ import numpy as np
 
 class StokesRadiusCalculator:
     @staticmethod
-    def stokes_einstein_radius(D):
+    def stokes_einstein_radius(D,T,eta):
         """
         Calculate the radius of a particle from the diffusion coefficient using the Stokes-Einstein equation.
 
         Parameters:
         D (float): Diffusion coefficient (cm²/s)
         T (float): Absolute temperature (K)
-        eta (float): Dynamic viscosity of the fluid (Pa·s)
+        eta (float): Dynamic viscosity of the fluid (cP)
 
         Returns:
         float: Radius of the particle (m)
         """
         # Boltzmann constant in J/K
-        T = 298.15  # Absolute temperature in Kelvin
-        eta = 0.334*0.001  # Dynamic viscosity of acetonitrile at 25°C in Pa·s (or kg/(m·s))
         k_B = 1.380649e-23
+        # Convert dynamic viscosity from cP to kg/(m·s) (or Pa·s)
+        eta = eta*0.001
         
         # Convert diffusion coefficient from cm²/s to m²/s
         D = D * 1e-4
@@ -31,7 +31,9 @@ class StokesRadiusCalculator:
 class DiffusivityCalculator:
     @staticmethod
     def wilke_chang_diffusion_coefficient(molar_volume, molecular_weight, temp, viscosity, alpha):
-        return (7.4E-8) * temp * np.sqrt(alpha * molecular_weight) / (viscosity * (alpha * molar_volume)**0.6)
+        d = (7.4E-8) * temp * np.sqrt(alpha * molecular_weight) / (viscosity * (alpha * molar_volume)**0.6)
+        d = float(d) # The value was being printed like: [np.float64(5.497635472812708e-06), np.float64(4.708457138648548e-06), np.float64(4.194534214640366e-06), np.float64(3.7831496747983576e-06)]
+        return d
 
 
 
